a3aa78b4d1f3ae50c6cf1dbb62da98dfad1b5971
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-02-19 18:44:10 +0200

    Projekti 1 valmis

--- src/components/accordian/index.tsx
+++ src/components/accordian/index.tsx
@@ -1,42 +1,93 @@
-import React, { useState } from "react";
-import data from "./data";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState } from 'react';
+import data from './data';
 import './styles.css';
 
 const Accordian: React.FC = () => {
-    const [selected, setSelected] = useState<string | null>(null);
-    const [enableMultiSelection, setEnableMultiSelection] = useState<boolean>(false);
-    const [multipleSelection, setMultipleSelection] = useState<[]>([]);
+  const [selected, setSelected] = useState<string | null>(null);
+  const [enableMultiSelection, setEnableMultiSelection] =
+    useState<boolean>(false);
+  const [multiple, setMultiple] = useState<string[]>([]);
 
-    function handleSingleSelection(dataId: string) {
-        console.log('dataId:', dataId)
-        setSelected(dataId === selected ? null : dataId);
+  function handleSingleSelection(dataId: string) {
+    console.log('dataId:', dataId);
+    setSelected(dataId === selected ? null : dataId);
+  }
+
+  function handleMultiSelection(dataId: string) {
+    //Luodaan kopio statesta, jotta voidaan muokata sitä React-sääntöjen mukaisesti
+    const copyMultiple: string[] = [...multiple];
+
+    const findIndexOfCurrentId = copyMultiple.indexOf(dataId);
+
+    if (findIndexOfCurrentId === -1 && dataId) {
+      copyMultiple.push(dataId);
+    } else if (findIndexOfCurrentId !== -1) {
+      copyMultiple.splice(findIndexOfCurrentId, 1);
     }
-    console.log('selected:', selected)
+    setMultiple(copyMultiple);
+  }
+  console.log('selected:', selected, 'multiple:', multiple);
 
+  return (
+    <div className='wrapper'>
+      <button
+        onClick={() =>
+          setEnableMultiSelection((prevValue) => (prevValue = !prevValue))
+        }
+        className='btn'
+      >
+        Enable Multi-Selection
+      </button>
 
-    return (
-        <div className="wrapper">
-            <button className="btn">Enable Multi-Selection</button>
+      <div className='accordian'>
+        {
+          // Tarkistetaan, onko data-array olemassa ja sisältääkö se elementtejä
+          data && data.length > 0 ? (
+            // Jos data-array on olemassa ja sisältää elementtejä, käydään läpi jokainen elementti
+            data.map((dataItem) => (
+              // Jokaiselle elementille luodaan div-elementti, jonka avain on elementin id
+              <div key={dataItem.id} className='item'>
+                {/* Tämän div-elementin klikkaaminen laukaisee joko handleMultiSelection- 
+                tai handleSingleSelection-funktion riippuen siitä, onko monivalinta käytössä */}
+                <div
+                  onClick={
+                    enableMultiSelection
+                      ? () => handleMultiSelection(dataItem.id)
+                      : () => handleSingleSelection(dataItem.id)
+                  }
+                  className='title'
+                >
+                  <h3>{dataItem.question}</h3>
+                  <span>+</span>
 
-           <div className="accordian">
-            {
-                data && data.length > 0 ? 
-                data.map(dataItem => (
-                    <div key={dataItem.id} className="item">
-                        <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
-                            <h3>{dataItem.question}</h3>
-                            <span>+</span>
-                            {selected === dataItem.id ?
-                                <div className="content">{dataItem.answer}</div>
-                                 : null }
-                        </div>
-                    </div>
-                )) :
-                <div>No data found</div>
-            }
-           </div>
-        </div>
-    );
-}
+                  {/* Jos monivalinta on käytössä ja elementin id löytyy multiple-arraysta, näytetään elementin vastaus
+                        Jos monivalinta ei ole käytössä ja elementin id on sama kuin valitun elementin id, 
+                        näytetään elementin vastaus */}
+                  {enableMultiSelection
+                    ? multiple.indexOf(dataItem.id) !== -1 && (
+                        <div className='content'>{dataItem.answer}</div>
+                      )
+                    : selected === dataItem.id && (
+                        <div className='content'>{dataItem.answer}</div>
+                      )}
+
+                  {/* // Jos elementin id on sama kuin valitun elementin id tai se löytyy multiple-arraysta, näytetään elementin vastaus
+        // Muussa tapauksessa ei näytetä mitään */}
+                  {selected === dataItem.id ||
+                  multiple.indexOf(dataItem.id) !== -1 ? (
+                    <div className='content'>{dataItem.answer}</div>
+                  ) : null}
+                </div>
+              </div>
+            ))
+          ) : (
+            <div>No data found</div>
+          )
+        }
+      </div>
+    </div>
+  );
+};
 
 export default Accordian;
